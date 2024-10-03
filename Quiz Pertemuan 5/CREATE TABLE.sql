263a9c60525ba506131d2bad4ec50b90d88db1f5
Syifa Nur Nabila <109137932+SyifaNurNabila@users.noreply.github.com>
2024-10-03 09:08:52 +0700

    Add files via upload

---
+++ Quiz Pertemuan 5/CREATE TABLE.sql
@@ -0,0 +1,4 @@
+CREATE TABLE invoices (
+    id INT PRIMARY KEY,
+    Total DECIMAL(10,2)
+);
---
+++ Quiz Pertemuan 5/SELECT FROM.sql
@@ -0,0 +1,5 @@
+SELECT Total, Total + 0.25 AS Total_dengan_biaya FROM invoices;
+SELECT Total, Total - 0.15 AS Total_dengan_diskon FROM invoices;
+SELECT Total, Total * 2 AS Total_dikali_dua FROM invoices;
+SELECT Total, Total / 2 AS Total_dibagi_dua FROM invoices;
+SELECT Total, Total % 2 AS Sisa_bagi FROM invoices;
